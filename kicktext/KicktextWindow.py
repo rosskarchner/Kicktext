208942dd028311a0fff6450f986b12eafcd2de0d
Ross Karchner <rosskarchner@gmail.com>
2013-06-14 08:07:18 -0400

    We now create temporary files
    Instead of keeping HTML purely in memory.

--- kicktext/KicktextWindow.py
+++ kicktext/KicktextWindow.py
@@ -1,6 +1,17 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# This file is in the public domain
+# Copyright (C) 2013 Ross Karchner ross@karchner.com
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 from locale import gettext as _
@@ -26,7 +37,7 @@ class KicktextWindow(Window):
         self.PreferencesDialog = PreferencesKicktextDialog
 
         # Code for other initialization actions should be added here.
-        self.controller = controllers.DocumentController()
+        self.controller = controllers.DocumentController(self)
         self.webview = WebKit.WebView()
         self.scroller = self.builder.get_object('scrollbox')
         self.scroller.add(self.webview)
--- kicktext/controllers.py
+++ kicktext/controllers.py
@@ -1,3 +1,7 @@
+import os
+import os.path
+import tempfile
+
 from gi.repository import Gio
 from engines import ENGINES
 
@@ -6,16 +10,35 @@ class DocumentController(object):
     last_mtime = 0
     path = None
     monitor = None
+    
+    def __init__(self, window):
+        self.window = window
+        window.connect('delete_event', self.close_files)
 
     def watch_file(self, path, webview):
         self.path =path
+        self.close_files()
+        self.dir = os.path.dirname(self.path)
         self.gfile = Gio.file_new_for_path(path)
         self.monitor = self.gfile.monitor(0,None)
+
+        self.scratch = tempfile.NamedTemporaryFile(dir=self.dir, prefix='.', suffix='.html')
+        self.scratch_url = "file://%s" % self.scratch.name
 
         def update_webview(*args):
             engine = ENGINES[self.engine_name]
             content = engine(self.path)
-            webview.load_string(content, 'text/html','UTF-8', 'http://localhost')
+            self.scratch.truncate(0)
+            self.scratch.write(content)
+            self.scratch.flush()
+            os.fsync(self.scratch.file.fileno())
+            webview.load_uri(self.scratch_url)
 
         update_webview()
+        webview.connect('load-error',update_webview)
         self.monitor.connect('changed', update_webview)
+
+    def close_files(self, *args):
+        if hasattr(self,'scratch') and not self.scratch.closed:
+            self.scratch.close()
+        return False
